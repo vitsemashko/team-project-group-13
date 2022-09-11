81fe1d6f7804de79cc3f34b75e72f8ea412ee5aa
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-09-11 11:23:49 +0000

    Deploying to gh-pages from @ vitsemashko/team-project-group-13@1388a69447821150145c4e6a28ae16340938f395 🚀

---
+++ index.ad4dd1a8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("myModal"),t=document.getElementById("myImg"),n=document.getElementById("img01"),o=document.getElementById("caption");document.getElementsByTagName("body");t.onclick=function(){document.body.classList.toggle("modal-open"),e.style.display="block",n.src=this.src,o.innerHTML=this.alt},document.getElementsByClassName("close")[0].onclick=function(){document.body.classList.toggle("modal-open"),e.style.display="none"}}();
+//# sourceMappingURL=index.ad4dd1a8.js.map
---
+++ index.e3fb8227.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("myModal"),t=document.getElementById("myImg"),n=document.getElementById("img01"),o=document.getElementById("caption");document.getElementsByTagName("body");t.onclick=function(){document.body.classList.toggle("modal-open"),e.style.display="block",n.src=this.src,o.innerHTML=this.alt},document.getElementsByClassName("close")[0].onclick=function(){document.body.classList.toggle("modal-open"),e.style.display="none"};
+//# sourceMappingURL=index.e3fb8227.js.map
