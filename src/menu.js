84d526b4ef03d3eb2937db8898f6a3c7d1ad8f8d
TetianaKuzma <tetiana.b97@gmail.com>
2022-09-13 18:22:45 +0300

    Upd header

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,24 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('.menu-open'),
-      closeMenuBtn: document.querySelector('.menu-close'),
-      menu: document.querySelector('.mob-menu'),
-      body: document.querySelector('body'),
-      menuList: document.querySelector('.mob-menu-list'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-    refs.menuList.addEventListener('click', removeMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle('visually-hidden');
-      refs.body.classList.toggle('no-scroll');
-      refs.body.classList.toggle('no-scroll');
-    }
-  
-    function removeMenu() {
-      refs.menu.classList.add('visually-hidden');
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open__btn'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-menu-list'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+    refs.menu.classList.toggle('visually-hidden');
+    refs.body.classList.toggle('no-scroll');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('visually-hidden');
+  }
+})();
