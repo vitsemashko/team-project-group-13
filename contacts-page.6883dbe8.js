401eac691f90abe423805aeafd444f67d7da6e54
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-09-14 08:46:49 +0000

    Deploying to gh-pages from @ vitsemashko/team-project-group-13@95caf60c51017d92df53d2e55107fed1e876df8d 🚀

--- contacts-page.6883dbe8.js
+++
@@ -1,2 +0,0 @@
-window.onclick=function(t){if(!t.target.matches(".dropbtn")){var n,o=document.getElementsByClassName("dropdown-content");for(n=0;n<o.length;n++){var s=o[n];s.classList.contains("show")&&s.classList.remove("show")}}};
-//# sourceMappingURL=contacts-page.6883dbe8.js.map
